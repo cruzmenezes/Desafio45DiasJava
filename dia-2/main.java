9ef19e6903314756fcabb61fda8847489ad8b2cc
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2024-12-25 17:55:54 -0300

    operadores matematicos

---
+++ dia-2/main.java
@@ -0,0 +1,22 @@
+public class main {
+    // operadores matematicos
+
+    public static void main(String[] args)
+    {
+        int num1 = 100 + 50;
+        int num2 = 100 - 50;
+        int num3 = 100 * 50;
+        int num4 = 100 / 50;
+        
+        
+
+
+        System.out.println("O resultado dos numeros é: "+ num1);
+        System.out.println("O resultado dos numeros é: "+ num2);
+        System.out.println("O resultado dos numeros é: "+ num3);
+        System.out.println("O resultado dos numeros é: "+ num4);
+      
+        
+        
+    }
+}
